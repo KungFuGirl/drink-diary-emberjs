e53fa5d74ae2ba86898f2ed94be457c873f1f702
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-28 15:21:06 -0700

    Made all fields of the form from this card to display in template. Doesn't set the value to model properly yet

--- app/components/soda-form.js
+++ app/components/soda-form.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
+const {
+  set
+} = Ember;
 
 export default Ember.Component.extend({
+  prompt: null,
+  optionValuePath: 'value',
+  optionLabelPath: 'label',
+  store: Ember.inject.service( 'store' ),
+  init() {
+    this._super( ...arguments );
+    set(this, 'countries', this.get( 'store' ).findAll( 'country' ));
+  },
+  actions: {
+    change: function () {
+      let selectedIndex = this.$('select')[0].selectedIndex;
+      let countries = this.get('countries');
+      
+      // decrement index by 1 if we have a prompt
+      let hasPrompt = !!this.get('prompt');
+      let countryIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+      let _selection = country[countryIndex];
+  
+      this.sendAction('willChangeAction', _selection);
+
+      if (this.get('optionValuePath')) {
+        this.set('selection', _selection[this.get('optionValuePath')]);
+      } else {
+        this.set('selection', _selection);
+      }
+
+      this.sendAction('didChangeAction', _selection);
+    }
+  }
 });
--- app/models/soda.js
+++ app/models/soda.js
@@ -3,14 +3,15 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   brand: DS.attr('string'),
-  producer: DS.attr('string'),
+  parentCompany: DS.attr('string'),
   originData: DS.attr('string'),
   country: DS.belongsTo('country'),
   isDiet: DS.attr('boolean'),
   isCaffeinated: DS.attr('boolean'),
   color: DS.attr('string'),
-  creator: DS.belongsTo('creator'),
-  lastEditor: DS.belongsTo('last-editor'),
+  // creator: DS.belongsTo('creator'),
+  // lastEditor: DS.belongsTo('last-editor'),
   photos: DS.hasMany('photo'),
   notes: DS.hasMany('note')
 });
+
