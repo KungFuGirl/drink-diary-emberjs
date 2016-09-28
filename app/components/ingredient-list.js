5ddde574990a150e349f8a93172aa75890f55261
bambery <lwszolek@gmail.com>
2016-09-28 16:50:49 -0700

    working with liquid-fire, but maybe this isn't the right way to go

--- app/components/ingredient-list.js
+++ app/components/ingredient-list.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   ingredients: [{ id: 1, name: 'carbonated water'}, { id: 2, name: 'high fructose corn syrup' }, { id: 3, name: 'caramel color'}, { id: 4,name: 'phosphoric acid' }],
-  summaryOrEdit: "summary",
+  summaryOrEdit: "ingredient-list-summary",
   actions: {
     toggleEdit: function() {
-      if (this.get('summaryOrEdit') === "summary") {
-        this.set('summaryOrEdit', 'edit');
+      if (this.get('summaryOrEdit') === "ingredient-list-summary") {
+        this.set('summaryOrEdit', 'ingredient-list-edit');
         console.log('toggled to edit');
       } else {
-        this.set('summaryOrEdit', 'summary');
+        this.set('summaryOrEdit', 'ingredient-list-summary');
         console.log('toggled to sum');
       }
 
--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,5 @@
 export default function() {
   this.transition(
-    this.childOf('#ingredient-list'),
     this.use('explode', {
       matchBy: 'data-ingredient-id',
       use: [ 'fly-to', {easing: 'spring'} ]
