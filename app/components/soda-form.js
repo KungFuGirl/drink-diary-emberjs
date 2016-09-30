724688e2ccb60109ab0a54cf9d188f022cf0f8cb
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-30 15:33:30 -0700

    Checkbox is working. Cleaned up the code.

--- app/components/soda-form.js
+++ app/components/soda-form.js
@@ -14,18 +14,15 @@ export default Ember.Component.extend({
   },
 
   actions: {
-      // setCountry(countryId) {
-      //   this.set('soda.country', this.get('store').peekRecord('country', countryId));
-      // },
       setCountry(country) {
         console.log(country);
         this.set('soda.country', country);
       },
-      // setDiet needs work
-      setDiet(isDiet) {
-        console.log(isDiet);
-        this.set('soda.isDiet', true);
+      setDiet(value) {
+        this.set('soda.isDiet', value);
+      },
+      setCaffeinated(value) {
+        this.set('soda.isCaffeinated', value);
       }
-
    }
 });
