dc6663a69503c96cd54f3711cbaa583886b963c0
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-09-30 19:18:04 -0700

    40 soda new form (#9)
    * Add photo model in order to be able to get to soda/:id page
    
    * Made all fields of the form from this card to display in template. Doesn't set the value to model properly yet
    
    * Able to set isDiet to true when boxed is checked. Tring to figure out how to set it to false when unchecked
    
    * Cleaned up not working code. Need to figure out change to false when unchecked and setting a country value
    
    * Debugging not working select country
    
    * Debugging set a country after selecting in soda form. Discovered shote.findRecord issue of not returning objects as expected
    
    * Made select work with native HTML5 select and peekRecord
    
    * Installed power-select, changed native select to power select for soda-form country drop down. Assigning country object to soda object works. Autocomplite doesn't
    
    * Checkbox is working. Cleaned up the code.
    
    * Add saveSoda function and save button to template. Ready to merge
    
    * Cleaned up code based on the provided review

--- app/components/soda-form.js
+++ app/components/soda-form.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
+const {
+  set
+} = Ember;
 
 export default Ember.Component.extend({
+  store: Ember.inject.service( 'store' ),
+  
+  init() {
+    this._super( ...arguments );
+    set(this, 'countries', this.get('store').findAll('country'));
+  },
+
+  actions: {
+      setCountry(country) {
+        this.set('soda.country', country);
+      },
+      setDiet(value) {
+        this.set('soda.isDiet', value);
+      },
+      setCaffeinated(value) {
+        this.set('soda.isCaffeinated', value);
+      },
+      saveSoda(soda) {
+        soda.save();
+      }
+   }
 });
