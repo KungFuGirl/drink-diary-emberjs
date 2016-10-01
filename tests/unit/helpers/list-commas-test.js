2e016b4e504af931dec73528b0f7bcdc3586de0e
bambery <lwszolek@gmail.com>
2016-09-30 17:12:15 -0700

    helper to insert commas in lists

---
+++ tests/unit/helpers/list-commas-test.js
@@ -0,0 +1,10 @@
+import { listCommas } from 'drink-diary/helpers/list-commas';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | list commas');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = listCommas([42]);
+  assert.ok(result);
+});
