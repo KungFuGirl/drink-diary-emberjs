99ea21b98bb90b25c4fe44d03b61522f6b4777c7
bambery <lwszolek@gmail.com>
2016-10-17 19:15:32 -0700

    whatever

---
+++ app/components/image-upload.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  resizeableImage: function(imageTarget) {
+    //var $container;
+//    var orig_src = new Image(),
+//      image_target = this.$(image_target).get(0),
+//      event_state = {},
+//      constrain = false,
+//      min_width = 60,
+//      min_height = 60,
+//      max_width = 800,
+//      max_height = 900,
+//      resize_canvas = document.createElement('canvas');
+  }
+
+  //resizeableImage($('.resize-image'));
+});
